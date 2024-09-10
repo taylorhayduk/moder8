874dc3358f8eed8c74e3688e268ca1c46bf25c28
Taylor Hayduk <taylorhayduk@gmail.com>
2024-09-10 12:44:25 -0400

    spacing on curl

--- app/components/CodeSelector.tsx
+++ app/components/CodeSelector.tsx
@@ -70,77 +70,77 @@ export default function CodeSelector() {
 
 const codeSnippets = [
   `curl https://gumroad-jiffy.vercel.app/api/moderate \\
-  -X POST \\
-  -H "Content-Type: application/json" \\
-  -d '{
-    "content": [
-      {
-        "type": "text",>
-        "text": "{\\"type\\":\\"product\\",\\"url\\":\\"goodsnooze.gumroad.com/l/vivid\\",\\"name\\":\\"Vivid - Double your MacBook Pro Brightness\\",\\"description\\":\\"Vivid doubles the brightness of your MacBook Pro across all apps, not just videos! ⚠️ Vivid only works on MacBook Pro with M1/2/3 Pro or Max chips.\\"}"
-      },
-      {
-        "type": "image_url",
-        "image_url": {
-          "url": "https://public-files.gumroad.com/bbaop6t7ewslyb1q4rdwssyf0yw1"
-        }
+-X POST \\
+-H "Content-Type: application/json" \\
+-d '{
+  "content": [
+    {
+      "type": "text",>
+      "text": "{\\"type\\":\\"product\\",\\"url\\":\\"goodsnooze.gumroad.com/l/vivid\\",\\"name\\":\\"Vivid - Double your MacBook Pro Brightness\\",\\"description\\":\\"Vivid doubles the brightness of your MacBook Pro across all apps, not just videos! ⚠️ Vivid only works on MacBook Pro with M1/2/3 Pro or Max chips.\\"}"
+    },
+    {
+      "type": "image_url",
+      "image_url": {
+        "url": "https://public-files.gumroad.com/bbaop6t7ewslyb1q4rdwssyf0yw1"
       }
-    ]
-    }'`,
+    }
+  ]
+}'`,
   `curl https://gumroad-jiffy.vercel.app/api/moderate \\
-  -X POST \\
-  -H "Content-Type: application/json" \\
-  -d '{
-    "content": [
-      {
-        "type": "text",
-        "text": "{\\"url\\":\\"pornhub.com\\",\\"content\\":\\"This is an adult website. This website contains age-restricted materials including nudity and explicit depictions of sexual activity. By entering, you affirm that you are at least 18 years of age or the age of majority in the jurisdiction you are accessing the website from and you consent to viewing sexually explicit content.\\"}"
-      },
-      {
-        "type": "image_url",
-        "image_url": {
-          "url": "https://api.url2png.com/v6/P4DF2F8BC83648/189f62d5d9da7d7308982fc5650fa4b3/png/?thumbnail_max_width=851&url=pornhub.com&viewport=1280x2000"
-        }
+-X POST \\
+-H "Content-Type: application/json" \\
+-d '{
+  "content": [
+    {
+      "type": "text",
+      "text": "{\\"url\\":\\"pornhub.com\\",\\"content\\":\\"This is an adult website. This website contains age-restricted materials including nudity and explicit depictions of sexual activity. By entering, you affirm that you are at least 18 years of age or the age of majority in the jurisdiction you are accessing the website from and you consent to viewing sexually explicit content.\\"}"
+    },
+    {
+      "type": "image_url",
+      "image_url": {
+        "url": "https://api.url2png.com/v6/P4DF2F8BC83648/189f62d5d9da7d7308982fc5650fa4b3/png/?thumbnail_max_width=851&url=pornhub.com&viewport=1280x2000"
       }
-    ]
-    }'`,
+    }
+  ]
+}'`,
   `curl https://gumroad-jiffy.vercel.app/api/moderate \\
-  -X POST \\
-  -H "Content-Type: application/json" \\
-  -d '{
-    "content": [
-      {
-        "type": "text",
-        "text": "{\\"type\\":\\"user\\",\\"url\\":\\"x.com/shl\\",\\"name\\":\\"@shl on Twitter\\",\\"posts\\":[\\"Follow me for updates on my new website :)\\",\\"Check out my new website at x.com! 🚀\\",\\"I'\\''m excited to announce the launch of my new website at x.com! 🎉\\"]}"
-      },
-      {
-        "type": "image_url",
-        "image_url": {
-          "url": "https://pbs.twimg.com/profile_images/1764285408135753728/pYT7qoCb_400x400.jpg"
-        }
+-X POST \\
+-H "Content-Type: application/json" \\
+-d '{
+  "content": [
+    {
+      "type": "text",
+      "text": "{\\"type\\":\\"user\\",\\"url\\":\\"x.com/shl\\",\\"name\\":\\"@shl on Twitter\\",\\"posts\\":[\\"Follow me for updates on my new website :)\\",\\"Check out my new website at x.com! 🚀\\",\\"I'\\''m excited to announce the launch of my new website at x.com! 🎉\\"]}"
+    },
+    {
+      "type": "image_url",
+      "image_url": {
+        "url": "https://pbs.twimg.com/profile_images/1764285408135753728/pYT7qoCb_400x400.jpg"
       }
-    ]
-  }'`,
+    }
+  ]
+}'`,
   `curl https://gumroad-jiffy.vercel.app/api/moderate \\
-  -X POST \\
-  -H "Content-Type: application/json" \\
-  -d '{
-      "content": [
-        {
-          "type": "image_url",
-          "image_url": {"url": "https://api.url2png.com/v6/P4DF2F8BC83648/189f62d5d9da7d7308982fc5650fa4b3/png/?thumbnail_max_width=851&url=pornhub.com&viewport=1280x2000"}
-        },
-        {
-          "type": "image_url",
-          "image_url": {"url": "https://pbs.twimg.com/profile_images/1764285408135753728/pYT7qoCb_400x400.jpg"}
-        },
-        {
-          "type": "text",
-          "text": "Like the shape of an l on your forehead, loser."
-        },
-        {
-          "type": "text",
-          "text": "Your mom is lovely."
-        }
-      ]
-  }'`,
+-X POST \\
+-H "Content-Type: application/json" \\
+-d '{
+  "content": [
+    {
+      "type": "image_url",
+      "image_url": {"url": "https://api.url2png.com/v6/P4DF2F8BC83648/189f62d5d9da7d7308982fc5650fa4b3/png/?thumbnail_max_width=851&url=pornhub.com&viewport=1280x2000"}
+    },
+    {
+      "type": "image_url",
+      "image_url": {"url": "https://pbs.twimg.com/profile_images/1764285408135753728/pYT7qoCb_400x400.jpg"}
+    },
+    {
+      "type": "text",
+      "text": "Like the shape of an l on your forehead, loser."
+    },
+    {
+      "type": "text",
+      "text": "Your mom is lovely."
+    }
+  ]
+}'`,
 ];
